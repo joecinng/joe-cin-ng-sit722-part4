36a2da8a9c7e6a40954dcead2b1c1b1e3c268b01
Joe Cin Ng <joe5763481@gmail.com>
2024-09-11 12:19:47 +1000

    first commit

---
+++ inventory_management/app/config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://joe_cin_ng_sit722_part_4_user:vdxglUaH0qc6jeMRfXGF3pw8e6JOL2bo@dpg-crg5fbjqf0us73dhkuo0-a.oregon-postgres.render.com/joe_cin_ng_sit722_part_4')
---
+++ variables.tf
@@ -0,0 +1,12 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+}
+
+variable location {
+  default = "eastus"
+}
+
+variable kubernetes_version {    
+  default = "1.30.2"
+}
